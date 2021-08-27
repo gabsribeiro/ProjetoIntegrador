f428a331cb03b9689295087b48cb71f3ece28f31
NicXavier <notedenic@gmail.com>
2021-08-27 10:04:20 -0300

    Criação do Projeto Integrador

---
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.Projeto.Integrador.Grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4Application.class, args);
+	}
+
+}
