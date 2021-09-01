844e9cf7e6322e34b0023fa9fad1d71be4448880
weslley silva <weslleysilvapaiva@outlook.com>
2021-09-01 14:55:41 -0300

    Method Put

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
@@ -1,18 +1,21 @@
 package com.Projeto.Integrador.Grupo4.Controller;
 
 import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Projeto.Integrador.Grupo4.Model.CategoryModel;
 import com.Projeto.Integrador.Grupo4.Repository.CategoryRepository;
-
 
 @CrossOrigin("*")
 @RestController
@@ -20,23 +23,28 @@ import com.Projeto.Integrador.Grupo4.Repository.CategoryRepository;
 public class CategoryController {
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryModel>> findAll(){
+	public ResponseEntity<List<CategoryModel>> findAll() {
 		List<CategoryModel> obj = repository.findAll();
 		return ResponseEntity.ok().body(obj);
-		
+
 	}
-	
-	@GetMapping (value = "/{id}")
-	public ResponseEntity<CategoryModel> findById(@PathVariable Integer id){
-		ResponseEntity<CategoryModel> obj = repository.findById((int) id).map(
-                resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryModel> findById(@PathVariable Integer id) {
+		ResponseEntity<CategoryModel> obj = repository.findById((int) id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 		return obj;
 	}
-	
-	@PostMapping 
-	public ResponseEntity<CategoryModel> post (@RequestBody CategoryModel category){
+
+	@PostMapping
+	public ResponseEntity<CategoryModel> post(@Valid @RequestBody CategoryModel category) {
 		return ResponseEntity.status(201).body(repository.save(category));
+	}
+
+	@PutMapping
+	public ResponseEntity<CategoryModel> update(@Valid @RequestBody CategoryModel updateCategory){
+		return ResponseEntity.status(201).body(repository.save(updateCategory));
 	}
 }
