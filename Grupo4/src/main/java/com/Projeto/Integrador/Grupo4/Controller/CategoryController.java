712f687571fbe34f4cce933f61a6e4843b54473e
NicXavier <notedenic@gmail.com>
2021-09-01 14:38:17 -0300

    Method Post

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Projeto.Integrador.Grupo4.Model.CategoryModel;
@@ -33,5 +35,8 @@ public class CategoryController {
 		return obj;
 	}
 	
-	
+	@PostMapping 
+	public ResponseEntity<CategoryModel> post (@RequestBody CategoryModel category){
+		return ResponseEntity.status(201).body(repository.save(category));
+	}
 }
