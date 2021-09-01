0693400cfcf0eb69a660a2a6cbd2c3dd19814f99
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-01 15:04:24 -0300

    Method deleteById

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,7 +45,12 @@ public class CategoryController {
 	}
 
 	@PutMapping
-	public ResponseEntity<CategoryModel> update(@Valid @RequestBody CategoryModel updateCategory){
+	public ResponseEntity<CategoryModel> update(@Valid @RequestBody CategoryModel updateCategory) {
 		return ResponseEntity.status(201).body(repository.save(updateCategory));
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Integer id) {
+		repository.deleteById(id);
 	}
 }
