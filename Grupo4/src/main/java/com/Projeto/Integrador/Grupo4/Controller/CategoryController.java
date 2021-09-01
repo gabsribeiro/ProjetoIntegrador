aea70090e9fa4ad39a291f72675edfb5dd4a7613
unknown <m_iaan@hotmail.com>
2021-09-01 14:25:11 -0300

    Find By Id

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Projeto.Integrador.Grupo4.Model.CategoryModel;
@@ -24,5 +25,13 @@ public class CategoryController {
 		return ResponseEntity.ok().body(obj);
 		
 	}
-
+	
+	@GetMapping (value = "/{id}")
+	public ResponseEntity<CategoryModel> findById(@PathVariable Integer id){
+		ResponseEntity<CategoryModel> obj = repository.findById((int) id).map(
+                resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		return obj;
+	}
+	
+	
 }
