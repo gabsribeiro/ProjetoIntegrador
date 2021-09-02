68fcfb02d6a6fe68b55f9b60e175db6e8a7e8c59
NicXavier <notedenic@gmail.com>
2021-09-02 17:24:39 -0300

    Data entry test

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/BookModel.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/BookModel.java
@@ -9,7 +9,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/config/TestConfig.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/config/TestConfig.java
@@ -1,10 +1,21 @@
 package com.Projeto.Integrador.Grupo4.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.Projeto.Integrador.Grupo4.service.DBService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
-
+	
+	@Autowired
+	private DBService service;
+	
+	
+	public void instanceDB() {
+		
+		this.service.instanceDB();
+	}
 }
