46921d2272d23d0317ab554cf73a12518f71566f
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-02 12:01:44 -0300

    Method update and deleteById

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/BookController.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/BookController.java
@@ -6,9 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +42,15 @@ public class BookController {
 	@PostMapping
 	public ResponseEntity<BookModel> post(@Valid @RequestBody BookModel book) {
 		return ResponseEntity.status(201).body(repository.save(book));
+	}
+	
+	@PutMapping
+	public ResponseEntity<BookModel> update(@Valid @RequestBody BookModel updateBook) {
+		return ResponseEntity.status(201).body(repository.save(updateBook));
+	}
+	
+	@DeleteMapping
+	public void deleteById(@PathVariable Integer id) {
+		repository.deleteById(id);
 	}
 }
