f503f22eff89e06b68fcf82eda77f3c8feaa5c9f
unknown <m_iaan@hotmail.com>
2021-09-02 13:15:59 -0300

    Foreign Key and join tables

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/BookModel.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/BookModel.java
@@ -4,10 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "book")
@@ -50,6 +54,10 @@ public class BookModel {
 	@NotNull(message = "Informe a quantidade páginas")
 	private int pages;
 	
+	@ManyToOne
+	@JoinColumn (name = "IdCategory")
+	@JsonIgnoreProperties ({"book"})
+	private CategoryModel category;
 
 	public int getIdProduct() {
 		return idProduct;
@@ -145,6 +153,14 @@ public class BookModel {
 
 	public void setPages(int pages) {
 		this.pages = pages;
+	}
+
+	public CategoryModel getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryModel category) {
+		this.category = category;
 	}
 
 	
--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/CategoryModel.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Model/CategoryModel.java
@@ -1,12 +1,19 @@
 package com.Projeto.Integrador.Grupo4.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "category")
@@ -31,6 +38,11 @@ public class CategoryModel {
 	public int getIdCategory() {
 		return idCategory;
 	}
+	
+	@OneToMany (mappedBy = "category", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ({"category"})
+	private List<BookModel> book = new ArrayList<>();
+	
 
 	public void setIdCategory(int idCategory) {
 		this.idCategory = idCategory;
@@ -68,4 +80,13 @@ public class CategoryModel {
 		this.classification = classification;
 	}
 
+	public List<BookModel> getBook() {
+		return book;
+	}
+
+	public void setBook(List<BookModel> book) {
+		this.book = book;
+	}
+	
+	
 }
