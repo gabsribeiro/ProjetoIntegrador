94d0e39c5d02f0f1d1966ee097c7caf1a39a42a0
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-10 11:02:53 -0300

    [Feature Gabriela - Task: Query findByEmail add to UserRepository]

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Repository/UserRepository.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.Projeto.Integrador.Grupo4.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Projeto.Integrador.Grupo4.Model.UserModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+	@Query("SELECT obj FROM UserModel obj WHERE UPPER(obj.email) LIKE CONCAT('%',UPPER(:email),'%')")
+	UserModel findByEmail(@Param("email") String email);
 
 }
