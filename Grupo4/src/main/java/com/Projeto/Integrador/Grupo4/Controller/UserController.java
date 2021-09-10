50075271163f09c047498da06e8fb2a91b3b1f42
NicXavier <notedenic@gmail.com>
2021-09-10 10:10:03 -0300

    [Feature Xavier - Task: create class UserController]

---
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.Projeto.Integrador.Grupo4.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@CrossOrigin ("*")
+@RequestMapping("/user")
+public class UserController {
+	
+
+	
+
+}
