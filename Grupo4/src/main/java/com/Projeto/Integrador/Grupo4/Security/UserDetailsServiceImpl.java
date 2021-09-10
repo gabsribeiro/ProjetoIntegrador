6833037daca453d6e31521ca459c4660bedc9d79
unknown <m_iaan@hotmail.com>
2021-09-10 12:31:03 -0300

    [Feature Mariana - task: Update UserDetailsImpl]

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/UserDetailsServiceImpl.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/UserDetailsServiceImpl.java
@@ -1,5 +1,33 @@
 package com.Projeto.Integrador.Grupo4.Security;
 
-public class UserDetailsServiceImpl {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.Projeto.Integrador.Grupo4.Model.UserModel;
+import com.Projeto.Integrador.Grupo4.Repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String Username) throws UsernameNotFoundException{
+		Optional<UserModel> objectOptional = repository.findByEmail(Username);
+		if (objectOptional.isPresent()) {
+			return new UserDetailsImpl(objectOptional.get());
+		}
+		
+		else {
+			throw new UsernameNotFoundException(Username + "nao existe");
+		}
+	
+	}
 
 }
