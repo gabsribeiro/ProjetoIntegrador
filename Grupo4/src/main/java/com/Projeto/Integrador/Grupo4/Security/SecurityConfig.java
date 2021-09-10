9efae572076d9947a936db87ca629c81e59ed678
unknown <m_iaan@hotmail.com>
2021-09-10 12:11:46 -0300

    [Feature Mariana - taks: Create method CorsConfigurationSource]

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/SecurityConfig.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.Projeto.Integrador.Grupo4.Security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,11 +22,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsServiceImpl service;
 	
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception{
+		//Liberar o aplicativo de gerenciamento do banco de dados h2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+    }
+
+    http.authorizeRequests()
+    .antMatchers(HttpMethod.POST,"/usuarios/login").permitAll()
+    .antMatchers(HttpMethod.POST,"/usuarios").permitAll()
+    .anyRequest().authenticated()
+    .and().httpBasic()
+    .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    .and().cors()
+    .and().csrf().disable();
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return (CorsConfigurationSource) source;
+    }
 	
 	
 	
