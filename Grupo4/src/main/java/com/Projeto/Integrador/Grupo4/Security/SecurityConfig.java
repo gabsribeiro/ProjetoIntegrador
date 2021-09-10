6a08b9586d543c8877390e99ff7bc8f7ecab7c62
NicXavier <notedenic@gmail.com>
2021-09-10 10:05:28 -0300

    [Feature Xavier - Task: update class 1.0 SecurityConfid]

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/SecurityConfig.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/Security/SecurityConfig.java
@@ -1,7 +1,25 @@
 package com.Projeto.Integrador.Grupo4.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsServiceImpl service;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	
 }
+ 
