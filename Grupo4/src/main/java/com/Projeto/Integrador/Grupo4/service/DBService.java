e85ff4967f0935a77a42398c69f3204556349d1a
Julio Cesar Bonfim <ferreira_bonffim@hotmail.com>
2021-09-14 12:02:57 -0300

    [feature: Julio task:Update BookModel, CategoryModel and DBService]

--- Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/service/DBService.java
+++ Grupo4/src/main/java/com/Projeto/Integrador/Grupo4/service/DBService.java
@@ -21,25 +21,27 @@ public class DBService {
 	
 	public void instanceDB() {
 	
-		CategoryModel c1 = new CategoryModel("Aventura/Sátira/Romance psicológico","Moderna","EPub",9);
-		CategoryModel c2 = new CategoryModel("Aventura/Fantasia"," Vide Editorial", "Livro",12);
-		CategoryModel c3 = new CategoryModel("Aventura/Ação/Mistério/Suspense","Arqueiro","eBook",14);
-		CategoryModel c4 = new CategoryModel("Aventura/Romance/Ficção Marítima","Harper Perennial","ePub",10);
-		CategoryModel c5= new CategoryModel("Aventura/Romance/Suspense/Ficção Científica","Rocco Digital","eBook",12);
+		CategoryModel c1 = new CategoryModel("Aventura");
+		CategoryModel c2 = new CategoryModel("Programação");
+		CategoryModel c3 = new CategoryModel("História");
+		CategoryModel c4 = new CategoryModel("Terror");
+		CategoryModel c5 = new CategoryModel("Biografia");
+		CategoryModel c6 = new CategoryModel("Ficção");
+		
 		BookModel b1 = new BookModel("Dom Quixote"," Apaixonado por histórias de cavalaria, Alonso Quijano "
 				+ "passa a acreditar que é um cavaleiro andante. Em seu delírio, muda o nome para Dom Quixote"
 				+ " de la Mancha, veste-se com uma armadura improvisada, faz de Dulcinéia sua amada, a quem quer"
 				+ " dedicar suas glórias e seus feitos. O vizinho Sancho Pança torna-se seu fiel escudeiro. "
 				+ "Nenhum cavaleiro andante teve a ousadia de Dom Quixote. Tampouco viveu suas aventuras e "
 				+ "desventuras, que aqui são contadas de forma divertida e emocionante.",28.90,"MIGUEL DE CERVANTES"
-				+ " | WALCYR CARRASCO",2009,50,"Português",9788516083373l,2000175600256l,"Brasil",100,c1);
+				+ " | WALCYR CARRASCO",2009,50,"Português",9788516083373l,2000175600256l,"Brasil","Moderna","Digital",100,c1);
 		BookModel b2 = new BookModel("Alice no País das Maravilhas","Uma menina, um coelho e uma história capazes de "
 				+ "fazer qualquer um de nós voltar a sonhar. Alice é despertada de um leve sono ao pé de uma árvore "
 				+ "por um coelho peculiar. Uma criatura alva e falante com roupas engraçadas, que consulta seu relógio"
 				+ " e reclama do próprio atraso. Curiosa como toda criança, Alice segue o animal até cair em um buraco"
 				+ " sem fim que mudou para sempre a literatura infantil. Mais de 150 anos depois, Alice no País das Maravilhas"
 				+ " continua repleto de ensinamentos para aqueles que ousaram seguir o Coelho Branco até sua toca.",
-				69.90,"LEWIS CARROLL",2021,48,"Português",65898750301l,9786589875031l,"Brasil",150,c2);
+				69.90,"LEWIS CARROLL",2021,48,"Português",65898750301l,9786589875031l,"Brasil"," Vide Editorial","Fisico",150,c1);
 		BookModel b3 = new BookModel("O Código da Vinci","Um assassinato dentro do Museu do Louvre, em Paris, traz à tona uma "
 				+ "sinistra conspiração para revelar um segredo que foi protegido por uma sociedade secreta desde os tempos de "
 				+ "Jesus Cristo. A vítima é o respeitado curador do museu, Jacques Saunière, um dos líderes dessa antiga fraternidade, "
@@ -50,7 +52,7 @@ public class DBService {
 				+ "Sophie e Robert vão à procura de pistas ocultas nas obras de Da Vinci e se debruçam sobre alguns dos maiores mistérios da "
 				+ "cultura ocidental - da natureza do sorriso da Mona Lisa ao significado do Santo Graal. Mesclando os ingredientes de um "
 				+ "envolvente suspense com informações sobre obras de arte, documentos e rituais secretos, Dan Brown consagrou-se como um dos "
-				+ "autores mais brilhantes da atualidade.",23.99,"Dan Brown",2012,37,"Português",9788580410853l,2008900670284l,"Brasil",432,c3);
+				+ "autores mais brilhantes da atualidade.",23.99,"Dan Brown",2012,37,"Português",9788580410853l,2008900670284l,"Brasil","Arqueiro","Digital",432,c1);
 		BookModel b4 = new BookModel("Anjos e Demônios","Antes de decifrar O Código Da Vinci, Robert Langdon, o famoso professor de simbologia "
 				+ "de Harvard, vive sua primeira aventura em Anjos e demônios, quando tenta impedir que uma antiga sociedade secreta destrua a "
 				+ "Cidade do Vaticano. Às vésperas do conclave que vai eleger o novo Papa, Langdon é chamado às pressas para analisar um "
@@ -65,19 +67,19 @@ public class DBService {
 				+ "desvendam enigmas e seguem uma trilha que pode levar ao covil dos Illuminati - um refúgio secreto onde está a única esperança "
 				+ "de salvação da Igreja nesta guerra entre ciência e religião. Em Anjos e Demônios, Dan Brown demonstra novamente sua habilidade"
 				+ " de entremear suspense com fascinantes informações sobre ciência, religião e história da arte.",21.99,"Dan Brown",2012,35,
-				"Português",9788580410860l,2020139322055l,"Brasil",616,c3);
+				"Português",9788580410860l,2020139322055l,"Brasil","Harper Perennial","Digital",616,c1);
 		BookModel b5 = new BookModel("Inferno","No coração da Itália, Robert Langdon, o professor de Simbologia de Harvard, é arrastado para um mundo "
 				+ "angustiante centrado em uma das obra literárias mais duradouras e misteriosas da história: O Inferno, de Dante Alighieri. Numa "
 				+ "corrida contra o tempo, Langdon luta contra um adversário assustador e enfrenta um enigma engenhoso que o arrasta para uma clássica"
 				+ " paisagem de arte, passagens secretas e ciência futurística. Tendo como pano de fundo o sombrio poema de Dante, Langdon mergulha "
 				+ "numa caçada frenética para encontrar respostas e decidir em quem confiar, antes que o mundo que conhecemos seja destruído.",21.99,
-				"Dan Brown",2013,31,"Português",9788408118077l,2008908084922l,"Brasil",448,c3);
+				"Dan Brown",2013,31,"Português",9788408118077l,2008908084922l,"Brasil","Rocco Digital","Digital",448,c1);
+		BookModel b6 = new BookModel("Raízes do Brasil", "Raízes do Brasil é uma das obras fundadoras do pensamento sobre a sociedade brasileira.",
+				80.99,"Sérgio Buarque de Holanda",2016,39,"Português",9788543806648l,854380664658l,"Brasil","Companhia das Letras","Digital",544,c3);
 		
-		c1.getBook().add(b1);
 		
-		
-		cRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5));
-		bRepository.saveAll(Arrays.asList(b1,b2,b3,b4,b5));
+		cRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5,c6));
+		bRepository.saveAll(Arrays.asList(b1,b2,b3,b4,b5,b6));
 	}
 	
 	
